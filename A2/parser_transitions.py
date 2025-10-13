2cd4d38e19cb47b0fb0a3437b889c1e28be286b7
BurnManBurn <palesadi10@gmail.com>
2025-10-14 00:40:23 +0330

    continuation

--- A2/parser_transitions.py
+++ A2/parser_transitions.py
@@ -32,8 +32,8 @@ class PartialParse(object):
         ### Note: The root token should be represented with the string "ROOT"
         ### Note: If you need to use the sentence object to initialize anything, make sure to not directly 
         ###       reference the sentence object.  That is, remember to NOT modify the sentence object. 
-        self.stack : list[str] = ["ROOT"]
-        self.buffer : list[str] = sentence.copy
+        self.stack : list[str] = ["<ROOT>"]
+        self.buffer : list[str] = sentence.copy()
         self.dependencies : list[tuple[str, str]] = []
         ### END YOUR CODE
 
@@ -52,16 +52,16 @@ class PartialParse(object):
         ###         1. Shift
         ###         2. Left Arc
         ###         3. Right Arc
-        assert transition in {"S", "LA", "RA"}, "Invalid transition is passed"
+        assert transition == "S" or "LA" or "RA"
 
-        if transition == "S":
-            self.stack.append(self.buffer.pop[0])
-        elif transition == "LA":
-            self.dependencies.append((self.stack.pop(-2), self.stack.pop(-1)))
-        elif transition == "RA":
-            self.dependencies.append((self.stack.pop(-1), self.stack.pop(-2)))
+        if transition == "S"
+            self.stack.append(self.buffer.pop(0))
+        elif transition == "LA"
+            self.dependencies.append((self.stack[-1], self.stack.pop(-2)))
+        elif transition == "RA"
+            self.dependencies.append((self.stack[-2], self.stack.pop(-1)))
         else:
-            raise ValueError('''Transition must be S, LA, or RA''')
+            raise ValueError(""" Transition must be "S", "LA", "RA" """)
 
         ### END YOUR CODE
 
@@ -77,7 +77,6 @@ class PartialParse(object):
         for transition in transitions:
             self.parse_step(transition)
         return self.dependencies
-
 
 def minibatch_parse(sentences, model, batch_size):
     """Parses a list of sentences in minibatches using a model.
@@ -113,27 +112,23 @@ def minibatch_parse(sentences, model, batch_size):
     ###             to remove objects from the `unfinished_parses` list. This will free the underlying memory that
     ###             is being accessed by `partial_parses` and may cause your code to crash.
 
-    def is_terminated(pp: PartialParse) -> bool:
-        """Checks if the given PartialParse is terminated."""
-        return True if (len(pp.stack) == 1 and len(pp.buffer) == 0) else False
+
+    def _is_finished(pp: PartialParse) -> bool:
+        return True if (len(pp.stack) == 1) and (len(pp.buffer) == 0) else False
 
     partial_parses = [PartialParse(sentence) for sentence in sentences]
     unfinished_parses = partial_parses[:]
     while len(unfinished_parses) > 0:
         minibatch = unfinished_parses[:batch_size]
-        transitions = model.predict(minibatch)
-
-
+        transitions = model.predict(partial_parses)
         for pp, transition in zip(minibatch, transitions):
             pp.parse_step(transition)
-            if is_terminated(pp):
+            if _is_finished(pp):
                 unfinished_parses.remove(pp)
+    dependencies = [pp.dependencies]
 
-    dependencies = [pp.dependencies for pp in partial_parses]
     ### END YOUR CODE
-
     return dependencies
-
 
 def test_step(name, transition, stack, buf, deps,
               ex_stack, ex_buf, ex_deps):
